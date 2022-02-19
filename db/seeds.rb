d4b96c3a0d6f3cae001fb498f9aae69885e12ee5
Sid Saythongphet <sid.saythongphet@gmail.com>
2022-02-19 16:54:44 -0500

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,16 @@
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+10.times do
+    first = Faker::Name.first_name
+    last = Faker::Name.last_name
+    username = last + first
+    User.create(first_name: first, last_name: last, username: username, password: "12345" )
+end 
+
+5.times { Club.create(club_title: "#{Faker::Hobby.activity} Club", description: Faker::Lorem.paragraph(sentence_count: 4)) }
+
+(1..10).each { |i| UserClub.create(user_id: i, club_id: rand(1..Club.all.length)) }
+
+5.times { Event.create(event_title: Faker::Lorem.sentence(word_count: 3), event_date: Faker::Date.forward(days: 14), club_id: rand(1..Club.all.length)) }
 
 puts "✅ Done seeding!"
