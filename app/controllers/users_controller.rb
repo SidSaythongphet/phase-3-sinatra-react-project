8cb530084d06883c560ac4a08aae65cc6438f61e
Sid Saythongphet <sid.saythongphet@gmail.com>
2022-02-19 20:09:51 -0500

    create post user route and user by id route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,30 @@ class UsersController < ApplicationController
     
 # Create User Route
     post '/users' do
-            
+        user = User.create(
+            first_name: params[:first_name],
+            last_name: params[:last_name],
+            username: params[:username],
+            password: params[:password],
+        )
+        user.to_json
     end
 
 # Read User Route
     get '/users' do
         @users = User.all
         @users.to_json(include: [:clubs, :events])
+    end
+
+    get '/users/:id' do
+        find_user
+        @user.to_json(include: [:clubs, :events])
+    end
+    
+    private
+    
+    def find_user
+        @user = User.find_by_id(params[:id])
     end
 
 end
