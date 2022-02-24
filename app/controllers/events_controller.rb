6c5df5a98766b727bf67050cdf80ef7d3a0c71bc
Sid Saythongphet <sid.saythongphet@gmail.com>
2022-02-23 20:51:01 -0500

    create patch for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,17 @@ class EventsController < ApplicationController
 
 # Create Event Route
 
+    post '/events' do
+        club = Club.find_by_id(params[:club_id])
+        event = club.events.build(
+            event_title: params[:event_title],
+            event_date: params[:event_date]
+        )
+        if event.save
+            club.events << event
+            event.to_json
+        end 
+    end
 
 
 # Read Events Route
@@ -10,6 +21,10 @@ class EventsController < ApplicationController
         @events.to_json(include: [:users, :events])
     end
 
+    get '/clubs/:id/events' do
+        events = Event.where('club_id = ?', params[:id])
+        events.to_json
+    end
 
 # Update Event Route
 
